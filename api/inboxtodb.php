3fc4bd24740d08c176da7572396cc6a7b5a5ce40
ertegun <ertegun@users.noreply.github.com>
2020-03-09 22:22:06 +0300

    asdads

--- api/inboxtodb.php
+++ api/inboxtodb.php
@@ -20,17 +20,55 @@ foreach ($threads as $key => $value) :
   $mesaj = $inbox['thread']['last_permanent_item'];
   $mesaj['message_id'] = $mid;
   $mesaj['status'] = 0;
+  $recipients =
+    [
+      'users' => [$mesaj['user_id']] // must be an [array] of valid UserPK IDs | 1596158719=ertegunfidan
+    ];
   // var_dump($mesaj);
   // exit;
   #sohbetteki son mesajları sisteme kaydediyorum
   try {
 
-    if ($mesaj['user_id'] != $my_user_id) { //son mesaj benim değilse sisteme ekle
-      $insertOneResult = $collection_MesajKutusu->insertOne($mesaj);
+    $inserted_count = 0;
+    echo $item_type = $mesaj['item_type'];
+    if ($item_type == 'profile') {
+      $profile_id = $mesaj['profile']['pk'];
+      $result = $ig->people->follow($profile_id);
+      $sendMsg = $ig->direct->sendText($recipients, 'Takip isteği gönderildi');
+      continue;
     }
 
-    // $inserted_id = $insertOneResult->getInsertedId();
-    $inserted_count = $insertOneResult->getInsertedCount();
+    if ($item_type == 'placeholder') {
+      $text = $mesaj['placeholder']['message'];
+      if (strpos($text, 'has a private account')) {
+        //Takip edilmeyen sayfa,takip edeceğiz
+        $temp_str = explode("@", $text, 2)[1];
+        $user_name = explode(" ", $temp_str, 2)[0]; //Takip edilecek sayfanın adı
+        $userId = $ig->people->getUserIdForName($user_name); //takip edilecek sayfanın id si
+        // $ig->people->follow($userId); 
+        //id ile sayfayı takip ediyoruz.
+        $msg = "B001. Ne yazık ki @$user_name sayfasını takip etmediğim için videoyu şuan çeviremiyorum."
+          . "Ama merak etme @$user_name sayfasına takip isteği gönderdim.Daha sonra tekrar dene";
+        $ig->direct->sendText($recipients, $msg);
+        // $ig->direct->hideThread($mid); //Tüm konuşmayı siler
+      }
+
+      if (strpos($text, 'This post is unavailable because it was deleted')) {
+        #siliniş mesaj.işlem yapılmayacak
+      }
+
+      continue;
+    }
+
+    if ($item_type == 'text') {
+      #text mesaj bir işlem yapılmayacak
+      continue;
+    }
+    if ($mesaj['user_id'] != $my_user_id) { //son mesaj benim değilse sisteme ekle
+      $insertOneResult = $collection_MesajKutusu->insertOne($mesaj);
+      // $inserted_id = $insertOneResult->getInsertedId();
+      $inserted_count = $insertOneResult->getInsertedCount();
+    }
 
     if ($inserted_count) {
       $response['thread_' . $key] = $mid;
--- api/sender.php
+++ api/sender.php
@@ -16,7 +16,8 @@ try {
     var_dump($item_id, $document->user_id);
     $post_item_url = "$serverUrl/post/$item_id";
     if ($document->user_id != $my_user_id) {
-      $sendMsg = $ig->direct->sendText($recipients, $post_item_url);     }
+      $sendMsg = $ig->direct->sendText($recipients, $post_item_url);
+    }
     // var_dump($sendMsg);
     $update = $collection_MesajKutusu->updateOne(
       ['item_id' => $document->item_id],
